ee79cd3a54341be24c3dc88e9e9da1a6408a0ad6
tyvoiax <eadjeifrimpong@worcester.edu>
2017-04-05 19:45:52 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World!, ty is here!!';
 
-document.querySelector('img').onclick = function() {
-  alert('this is a computer circuit');
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/circuit.jpg') {
+    myImage.setAttribute('src', 'images/braincircuit.jpg');  
+  }
+  else {
+    myImage.setAttribute('src', 'images/circuit.jpg');  
+  }
 }
