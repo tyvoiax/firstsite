9be7962a7232c9745ced8f4abd0c00d8e547cad2
tyvoiax <eadjeifrimpong@worcester.edu>
2017-04-05 19:23:46 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World!, ty is here!!';
 
-document.querySelector('html').onclick = function() {
-  alert('Ouch!, stop poking me');
+document.querySelector('img').onclick = function() {
+  alert('this is a computer circuit');
 }
